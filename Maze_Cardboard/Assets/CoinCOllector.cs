9fba6d0a56fc7f1c636f66f567266456326b0ad6
meikub <44545393+meikub@users.noreply.github.com>
2020-12-21 16:38:53 +0100

    initial commit
    Its a maze game, that can be runned on Smartphone with a Cardboard

---
+++ Maze_Cardboard/Assets/CoinCOllector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCOllector : MonoBehaviour
+{
+	
+	private bool mFloorTouched = false;
+	void OnCollisionEnter(Collision coll){
+		if (coll.gameObject.tag.Equals ("Floor")) {
+			mFloorTouched = true;
+		}
+	}
+
+	void OnCollisionExit(Collision coll){
+		if (coll.gameObject.tag.Equals ("Floor")) {
+			mFloorTouched = false;
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag.Equals ("Coin")) {
+			Destroy(other.gameObject);
+		}
+	}
+}
